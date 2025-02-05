6e482a87b2f8adb82af7110fa1cdd1ff512aae01
ick-web <yih000098@gmail.com>
2025-02-05 11:57:42 +0900

    중간저장

--- src/components/todo/TodoForm.jsx
+++ src/components/todo/TodoForm.jsx
@@ -21,10 +21,10 @@ const TodoForm = ({ addTodos }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-        <input type="text" value={todoText} onChange={handleChangeTodoText} />
-        <button type="submit">제출하기</button>
-        </form>
-  )
-}
+      <input type="text" value={todoText} onChange={handleChangeTodoText} />
+      <button type="submit">제출하기</button>
+    </form>
+  );
+};
 
 export default TodoForm;
