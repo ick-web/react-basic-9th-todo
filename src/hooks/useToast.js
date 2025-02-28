bf2ab85ba458e7b5025200203c271eeb8ab55abc
ick-web <yih000098@gmail.com>
2025-02-28 17:22:15 +0900

    feat: useToast 훅 추가

---
+++ src/hooks/useToast.js
@@ -0,0 +1,35 @@
+import { immer } from "zustand/middleware/immer"
+import { create } from "zustand";
+
+export const useToast = create(
+    immer((set, get) => ({
+        toasts: [],
+    addToast: (message) => {
+    set((prevState) => {
+        const id = crypto.randomUUID();
+        const {removeToast} = get();
+
+        prevState.toasts.push({
+            id,
+            message,
+        });
+
+        setTimeout(() =>{
+            removeToast(id)
+        }, 3000)
+        })
+    },
+removeToast: (toastId) => {
+    set((prevState) => {
+        const targetIndex = prevState.toasts.findIndex(
+            (prevToast) => prevToast.id === toastId
+        );
+
+        if (targetIndex !== -1){
+            prevState.toasts.splice(targetIndex, 1);
+        }
+        });
+    },
+    }))
+);
+
