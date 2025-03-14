aad5ace317581e29a5627beb734415bcb514fdeb
ick-web <yih000098@gmail.com>
2025-03-14 19:21:36 +0900

    design: css 추가

--- components/todo/TodoList.tsx
+++ components/todo/TodoList.tsx
@@ -7,7 +7,7 @@ const TodoList = async () => {
   const todos = await getTodos();
 
   return (
-    <ul>
+    <ul className="space-y-2">
       {todos.map((todo) => (
         <li key={todo.id}>
           <TodoItem todo={todo} />
